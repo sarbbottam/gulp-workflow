1b25de2a019646c1588100cb3c715e29092a5abf
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2014-07-26 02:13:33 -0700

    updated gulpfile.js
    jshint task as per http://stackoverflow.com/questions/24966781/gulp-error-handling-for-dependent-task#answer-24969143
    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,18 @@ var uglify = require('gulp-uglify');
 gulp.task('jshint', function () {
   return gulp.src(['assets/js/**/*.js'])
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('uglify', ['jshint'], function() {
   return gulp.src('assets/js/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('assets-min/js/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('assets/js/**/*.js', ['jshint', 'uglify']);
 });
 
 gulp.task('default', ['jshint', 'uglify']);
